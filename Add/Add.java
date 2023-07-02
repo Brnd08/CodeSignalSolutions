2daf2398958a58a8d746f408e117ed694fe1b211
Brdn08 <93061195+Brnd08@users.noreply.github.com>
2023-07-01 18:28:45 -0600

    The images and source code to the Add coding challenge got added, also the markdown file got modified.

---
+++ Add/Add.java
@@ -0,0 +1,10 @@
+public class Add {
+        public static int solution(int param1, int param2){
+                return param1 + param2;
+        }
+        public static void main(String args[]){
+
+		System.out.println(" 1 + 1 == " + solution(1, 1));
+		System.out.println(" -1000 + -1000 == " + solution(-1000, -1000));
+        }
+}
---
+++ Add/Add.js
@@ -0,0 +1,6 @@
+function solution(param1, param2){
+	return param1 + param2;
+}
+
+console.log(" 1 + 1 == " + solution(1, 1))
+console.log(" -1000 + -1000 == " + solution(-1000, -1000))
