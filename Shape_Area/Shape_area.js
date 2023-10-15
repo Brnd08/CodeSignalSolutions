6f19761ea78899ecf3aa22a9d37ac9a7cb35a8f2
Nomarchess <94210909+NomarRodriguezH@users.noreply.github.com>
2023-10-15 13:22:52 -0600

    Delete Shape_Area directory

--- Shape_Area/Shape_area.js
+++
@@ -1,9 +0,0 @@
-function solution(n){
-	return 2 * n * (n - 1) + 1;
-}
-
-let test = 2
-let test1 = 100
-
-console.log("The area of a %d-interesting polygon is %d", test, solution(test))
-console.log(`The area of a ${test1}-interesting polygon is ${solution(test1)}`)
