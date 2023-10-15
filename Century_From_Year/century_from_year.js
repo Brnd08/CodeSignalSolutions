5183f6fa1329a30004d7228c22547207f726db60
Nomarchess <94210909+NomarRodriguezH@users.noreply.github.com>
2023-10-15 13:24:18 -0600

    Delete Century_From_Year directory

--- Century_From_Year/century_from_year.js
+++
@@ -1,6 +0,0 @@
-function solution (year){
-	return year%100==0? year/100 : parseInt((year/100) + 1);
-}
-
-console.log("The year 1905 in on the century number " + solution (1905));
-console.log("The year 1700 in on the century number " + solution (1700));
